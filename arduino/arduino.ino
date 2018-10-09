ac0593bdde54e2ec7bea55a4dbfb93462211d18f
Agustin Nieto <anietog1@eafit.edu.co>
2018-10-09 15:55:42 -0500

    Still not working, but prettier >:)

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,22 +1,49 @@
 #define debug_println(x) Serial.println(x)
 
+bool coordinates, route;
+byte const CRAZY_SIGNAL = '>';
+
 void setup() {
+  coordinates = route = false;
   Serial.begin(9600);
 }
 
-bool active = false, coordinates = false, route = false;
-byte const CRAZY_SIGNAL = '\000';
-
 void request_input() {
-  debug_println("sending signal");
   Serial.write(CRAZY_SIGNAL);
 }
 
 void serialEvent() {
-  char input[200] = {};
-  Serial.readBytesUntil('\n', input, 200);
-  Serial.print(input);
+  debug_println("input found!");
+
+  char input[256] = {};
+  Serial.readBytesUntil('\n', input, 256);
+
+  if (input[0] == CRAZY_SIGNAL) {
+    debug_println("CRAZY_SIGNAL found!");
+
+    if (coordinates) {
+      coordinates = false;
+      route = true;
+    } else if (route) {
+      route = false;
+    } else {//! coordinates && ! route
+      coordinates = true;
+    }
+  } else {
+    if (coordinates) {
+      //Do something
+      debug_println("coordinate");
+    } else if (route) {
+      //Do something
+      debug_println("point in route");
+    }
+  }
+
+  if (coordinates || route) {
+    request_input();
+  }
 }
 
 void loop() {
+  //Do nothing
 }
