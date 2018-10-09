7d1fc0ea4a29354d61892c5f5d9d2a8d8372c3c0
Agustin Nieto <anietog1@eafit.edu.co>
2018-10-09 13:20:40 -0500

    Basic arduino code. It's still not working as it should.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,44 @@
+#define debug_print(x) Serial.print(x)
+
 void setup() {
-  //
+  Serial.begin(9600);
+}
+
+bool active = false, coordinates = false, route = false;
+
+void serialEvent() {
+  byte input[200];
+
+  Serial.readBytesUntil('\n', input, 200);
+
+  if(active) {
+    if(coordinates) {
+      if(c[0] == '\000') {
+        coordinates = false;
+        route = true;
+      } else {
+        //Do something
+        debug_print("coordenada");
+      }
+    } else if(route) {
+      if(c[0] == '\000') {
+        route = false;
+        active = false;
+      } else {
+        //Do something
+        debug_print("punto en ruta");
+      }
+    } else {
+      if(c[0] == '\000') {
+        active = true;
+        coordinates = true;
+      }
+    }
+
+    Serial.write('\n');
+  }
 }
 
 void loop() {
-  //
+  //Do nothing
 }
