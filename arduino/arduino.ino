d88f65047e2eaacd7c65ec424137c3c7e0e64e64
Agustin Nieto <anietog1@eafit.edu.co>
2018-10-09 14:09:20 -0500

    Fixing some mistakes.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,41 +1,47 @@
-#define debug_print(x) Serial.print(x)
+#define debug_println(x) Serial.println(x)
 
 void setup() {
   Serial.begin(9600);
 }
 
 bool active = false, coordinates = false, route = false;
+byte const CRAZY_SIGNAL = '\000';
+
+void request_input() {
+  Serial.write(CRAZY_SIGNAL);
+}
 
 void serialEvent() {
   byte input[200] = {};
-
   Serial.readBytesUntil('\n', input, 200);
 
   if(active) {
     if(coordinates) {
-      if(c[0] == '\000') {
+      if(input[0] == CRAZY_SIGNAL) {
         coordinates = false;
         route = true;
       } else {
         //Do something
-        debug_print("coordinate");
+        debug_println("coordinate");
       }
+
+      request_input();
     } else if(route) {
-      if(c[0] == '\000') {
+      if(input[0] == CRAZY_SIGNAL) {
         route = false;
         active = false;
       } else {
         //Do something
-        debug_print("point in route");
+        debug_println("point in route");
+        request_input();
       }
     } else {
-      if(c[0] == '\000') {
+      if(input[0] == CRAZY_SIGNAL) {
         active = true;
         coordinates = true;
+        request_input();
       }
     }
-
-    Serial.write('\n');
   }
 }
 
